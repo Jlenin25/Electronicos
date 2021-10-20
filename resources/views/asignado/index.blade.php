fa880e0b3236e7d15b6abb0cc52e04d25be2a9c4
Jlenin25 <djlenin.55@gmail.com>
2021-10-19 20:39:16 -0500

    modificacion de admin lte

--- resources/views/asignado/index.blade.php
+++ resources/views/asignado/index.blade.php
@@ -8,7 +8,6 @@
     <thead>
         <tr>
             <th scope="col">ID</th>
-            <th scope="col">CODIGO ASIGNADO</th>
             <th scope="col">NOMBRE ASIGNADO</th>
             <th scope="col">ACCIONES</th>
         </tr>
@@ -17,7 +16,6 @@
         @foreach ($asignados as $asignado)
         <tr>
             <td>{{$asignado->id}}</td>
-            <td>{{$asignado->cod_asignado}}</td>
             <td>{{$asignado->name_asignado}}</td>
             <td>
                 <form action="{{route ('asignado.destroy',$asignado->id)}}" method="POST">
